2022b3d16a21c9b98644b40c7fc074f0fd315fcb
ayuub <ayoubbenomar1230@gmail.com>
2025-09-18 11:58:39 +0100

    refactor the maincontroller

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,12 @@
 import java.util.Scanner;
 import controller.MainController;
-import model.compte.Compte;
 
 public class Main {
-    public static void main(String[] arges){
-        Scanner sc = new Scanner(System.in);
-        MainController mc = new MainController();
-        boolean running = true;
+    private static Scanner sc = new Scanner(System.in);
+    private static MainController mc = new MainController();
 
-        while(running){
-            System.out.println("===> MENU BANQUE <===");
-            System.out.println("1. Créer un compte");
-            System.out.println("2. Consulter un compte");
-            System.out.println("3. Quitter");
-            System.out.print("Choisissez une option: ");
-
-            int choice = sc.nextInt();
-
-            switch (choice){
-                case 1:
-                    boolean running2 = true;
-                    while(running2){
-                        System.out.println("===> MENU CRÉER COMPTE <===");
-                        System.out.println("1. Créer un compte épargne");
-                        System.out.println("2. Créer un compte courant");
-                        System.out.println("3. Retour au menu précédent");
-                        System.out.print("Choisissez une option: ");
-
-                        switch (sc.nextInt()){
-                            case 1:
-                                mc.creeCompteEpargne();
-                                break;
-                            case 2:
-                                mc.creeCompteCourant();
-                                break;
-                            case 3:
-                                running2 = false;
-                                break;
-                            default:
-                                System.out.println("Veuillez entrer un choix");
-                        }
-                    }
-                    break;
-                case 2:
-                    System.out.println("Entrer votre compte code: ");
-                    String compteCode = sc.next();
-                    Compte compteTrouve = mc.trouverCompte(compteCode);
-                    if (compteTrouve != null){
-                        boolean running3 = true;
-                        while(running3){
-                            System.out.println("===> MENU CONSULTER COMPTE <===");
-                            System.out.println("1. Retirer");
-                            System.out.println("2. Faire un vairment");
-                            System.out.println("3. Afficher les opérations");
-                            System.out.println("4. Afficher les détails du compte");
-                            System.out.println("5. Retour au menu précédent");
-                            System.out.print("Choisissez une option: ");
-
-                            switch(sc.nextInt()){
-                                case 1:
-                                    System.out.println("Entrer montant: ");
-                                    int montant = sc.nextInt();
-                                    mc.retirer(compteCode, montant);
-                                    break;
-                            }
-
-                        }
-                    }
-            }
-        }
+    public static void main(String[] args) {
+        mc.runApplication(sc);
         sc.close();
     }
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,14 +1,148 @@
 package controller;
 
+import java.util.Scanner;
+import java.util.HashMap;
 import model.compte.Compte;
 import model.compte.CompteCourant;
 import model.compte.CompteEpargne;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class MainController {
     private HashMap<String, Compte> comptes = new HashMap<>();
+
+    public void runApplication(Scanner sc) {
+        boolean running = true;
+
+        while(running) {
+            showMainMenu();
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    handleCreateAccountMenu(sc);
+                    break;
+                case 2:
+                    handleConsultAccountMenu(sc);
+                    break;
+                case 3:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Veuillez entrer un choix valide");
+            }
+        }
+    }
+
+    private void showMainMenu() {
+        System.out.println("===> MENU BANQUE <===");
+        System.out.println("1. Créer un compte");
+        System.out.println("2. Consulter un compte");
+        System.out.println("3. Quitter");
+        System.out.print("Choisissez une option: ");
+    }
+
+    private void handleCreateAccountMenu(Scanner sc) {
+        boolean running2 = true;
+
+        while(running2) {
+            showCreateAccountMenu();
+
+            switch (sc.nextInt()) {
+                case 1:
+                    handleCreateSavingsAccount();
+                    break;
+                case 2:
+                    handleCreateCurrentAccount();
+                    break;
+                case 3:
+                    running2 = false;
+                    break;
+                default:
+                    System.out.println("Veuillez entrer un choix valide");
+            }
+        }
+    }
+
+    private void showCreateAccountMenu() {
+        System.out.println("===> MENU CRÉER COMPTE <===");
+        System.out.println("1. Créer un compte épargne");
+        System.out.println("2. Créer un compte courant");
+        System.out.println("3. Retour au menu précédent");
+        System.out.print("Choisissez une option: ");
+    }
+
+    private void handleCreateSavingsAccount() {
+        creeCompteEpargne();
+    }
+
+    private void handleCreateCurrentAccount() {
+        creeCompteCourant();
+    }
+
+    private void handleConsultAccountMenu(Scanner sc) {
+        System.out.println("Entrer votre compte code: ");
+        String compteCode = sc.next();
+        Compte compteTrouve = trouverCompte(compteCode);
+
+        if (compteTrouve != null) {
+            boolean running3 = true;
+
+            while(running3) {
+                showConsultAccountMenu();
+
+                switch(sc.nextInt()) {
+                    case 1:
+                        handleWithdraw(sc, compteCode);
+                        break;
+                    case 2:
+//                        handleDeposit(sc, compteCode);
+                        break;
+                    case 3:
+//                        handleShowOperations(compteCode);
+                        break;
+                    case 4:
+                        handleShowAccountDetails(compteCode);
+                        break;
+                    case 5:
+                        running3 = false;
+                        break;
+                    default:
+                        System.out.println("Veuillez entrer un choix valide");
+                }
+            }
+        } else {
+            System.out.println("Compte non trouvé!");
+        }
+    }
+
+    private void showConsultAccountMenu() {
+        System.out.println("===> MENU CONSULTER COMPTE <===");
+        System.out.println("1. Retirer");
+        System.out.println("2. Faire un versement");
+        System.out.println("3. Afficher les opérations");
+        System.out.println("4. Afficher les détails du compte");
+        System.out.println("5. Retour au menu précédent");
+        System.out.print("Choisissez une option: ");
+    }
+
+    private void handleWithdraw(Scanner sc, String compteCode) {
+        System.out.print("Entrer montant: ");
+        float montant = sc.nextFloat();
+        retirer(compteCode, montant);
+    }
+
+//    private void handleDeposit(Scanner sc, String compteCode) {
+//        System.out.print("Entrer le montant à déposer: ");
+//        float montant = sc.nextFloat();
+//        versement(compteCode, montant);
+//    }
+//
+//    private void handleShowOperations(String compteCode) {
+//        afficherOperations(compteCode);
+//    }
+
+    private void handleShowAccountDetails(String compteCode) {
+        afficherDetailsCompte(compteCode);
+    }
 
     public void creeCompteEpargne(){
         CompteEpargne ce = new CompteEpargne();
@@ -35,6 +169,33 @@ public class MainController {
 
     public void retirer(String code, float montant){
         Compte c = comptes.get(code);
-        c.retirer(montant);
+        if (c != null) {
+            c.retirer(montant);
+        }
+    }
+
+//    public void versement(String code, float montant){
+//        if (montant <= 0){
+//            System.out.println("❌ Montant invalide!");
+//            return;
+//        }
+//        Compte c = comptes.get(code);
+//        if (c != null) {
+//            c.versement(montant);
+//        }
+//    }
+//
+//    public void afficherOperations(String compteCode) {
+//        Compte c = comptes.get(compteCode);
+//        if (c != null) {
+//            c.afficherOperations();
+//        }
+//    }
+
+    public void afficherDetailsCompte(String compteCode) {
+        Compte c = comptes.get(compteCode);
+        if (c != null) {
+            System.out.println(c.toString());
+        }
     }
 }
