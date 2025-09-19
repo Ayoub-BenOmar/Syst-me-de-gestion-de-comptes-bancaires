bb6542a02e2e5fba2a571b1676e808f2434cdfb8
ayuub <ayoubbenomar1230@gmail.com>
2025-09-19 10:59:52 +0100

    added the calculating of the interet and add it to the initial balance

--- src/model/compte/CompteEpargne.java
+++ src/model/compte/CompteEpargne.java
@@ -1,15 +1,21 @@
 package model.compte;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class CompteEpargne extends Compte {
     private static double tauxInteret = 0.05;
+    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     public CompteEpargne() {
         super();
+        demarrerCalculInteret();
     }
 
     public void retirer(double montant){
         if(montant <= 0){
-            System.out.println("Le montant est ivalide.");
+            System.out.println("❌ Le montant est ivalide.");
             return;
         }
         if(montant <= solde){
@@ -24,7 +30,17 @@ public class CompteEpargne extends Compte {
         return solde * tauxInteret;
     }
 
+    public void demarrerCalculInteret(){
+
+        Runnable task = () ->{
+            double interet = calculerInteret();
+            setSolde(getSolde() + interet);
+        };
+
+        scheduler.scheduleAtFixedRate(task, 5, 5, TimeUnit.SECONDS);
+    }
+
     public String afficherDetails(){
-        return ("Compte Épargne [ Code: " + code + ", Solde: " + solde + ", Taux d'intérêt: " + tauxInteret + " ]");
+        return ("Compte Épargne [ Code: " + code + ", Solde: " + getSolde() + ", Taux d'intérêt: " + tauxInteret + " ]");
     }
 }
